602bc4843f3379bb8ae9850150463d02389d273b
DESKTOP-M51TMTU\Tejaswi <tejaswi.gillepally@gmail.com>
2019-04-21 16:19:13 -0400

    added springBoot java Project

---
+++ src/main/java/com/testspring/TestStaffService1/TestStaffService1Application.java
@@ -0,0 +1,13 @@
+package com.testspring.TestStaffService1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestStaffService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestStaffService1Application.class, args);
+	}
+
+}
